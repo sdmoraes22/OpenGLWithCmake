08bcdae5a0fd341b459ca0b94bb5d0cfcc23bc8e
Cristiano <cmoraes.ti@gmail.com>
2025-09-08 23:02:08 -0300

    Configuring OpenGl Uniforms

--- 02 - shaders/src/main.cpp
+++ 02 - shaders/src/main.cpp
@@ -20,10 +20,10 @@ const char *vertexShaderSource = "#version 330 core\n"
     "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
-    "in vec4 vertexColor;\n"
+    "uniform vec4 ourColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vertexColor;\n"
+    "   FragColor = ourColor;\n"
     "}\n\0";
 
 int main()
@@ -144,11 +144,15 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // draw our first triangle
+
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0) + 0.5f;
+        int vertgexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+
         glUseProgram(shaderProgram);
+        glUniform4f(vertgexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
         glDrawArrays(GL_TRIANGLES, 0, 3);
-        // glBindVertexArray(0); // no need to unbind it every time 
  
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
