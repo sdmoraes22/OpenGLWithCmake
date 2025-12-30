23a3c09bda14baf8602b86d3c783733913b6374a
Cristiano Moraes <cmoraes.ti@gmail.com>
2025-12-30 18:09:15 -0300

    Modificação dos arquivos de build
    Modificação dos arquivos de build

--- 02 - shaders/build.bat
+++ 02 - shaders/build.bat
@@ -1,9 +1,8 @@
-#!/bin/bash
-
-rm -rf build
+echo off
+cls
+echo "Iniciando o build"
+rmdir /S /Q build
 mkdir build
 cd build
 echo "Efetuando build do cmake"
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
-echo "Linkando"
-make
