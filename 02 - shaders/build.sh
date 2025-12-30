eef2805d743722085fa1fb6bddb1dfe9ed31739d
Cristiano Moraes <cmoraes.ti@gmail.com>
2025-12-30 14:00:57 -0300

    Remoção de logs desnecessários
    Remoção de logs não necessários

--- 02 - shaders/build.sh
+++ 02 - shaders/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-echo "Removendo pasta de build"
 rm -rf build
-echo "Criando pasta de build"
 mkdir build
-echo "Navegando para a pasta de build"
 cd build
 echo "Efetuando build do cmake"
 cmake ..
